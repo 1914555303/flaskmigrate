1c0befba78a3a6689ba0b84ec7c5b274aa350010
asus <1914555303@qq.com>
2022-08-23 21:29:12 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,158 @@
+from flask import Flask, render_template, request, url_for, redirect, flash ,session
+from ext import db
+import config
+from models import User, Category, Post
+import models
+
+# 目的：练习Flask-Migrate模块的使用
+# 安装依赖：  pip install flask-migrate
+# 其他依赖：
+# pip install pymysql
+# pip install SQLAlchemy
+# pip install flask-sqlalchemy
+# pip install flask-script
+
+# 迁移命令
+# 1.python manage.py db init
+# 2.python manage.py db migrate
+# 3.python manage.py db upgrade
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config.from_object(config)  # 载入config.py中的配置信息
+#
+#     db.init_app(app)  # app绑定数据库db
+#
+#     return app
+
+app = Flask(__name__)
+
+app.config.from_object(config)
+db.init_app(app)
+
+
+@app.route('/index')
+def index():
+
+    return render_template("index.html")
+
+
+@app.route('/login',  methods=['GET', 'POST'])
+def loginpage():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter(User.username == username).first()
+    # pwd = user.password
+    print(user)
+
+    if user is None:
+        errno = '用户名或密码错误！'
+        return render_template('login.html', errnos=errno)
+    else:
+        if user.username == username and user.password == password:
+            session['id'] = user.id
+            session['username'] = user.username
+            # return render_template('loginsuccess.html', username=username)
+            return redirect(url_for('zhuye'))
+        else:
+            errno = '用户名或密码错误！'
+            return render_template('login.html', errnos=errno)
+
+@app.route('/zhuye')
+def zhuye():
+    username = session.get('username')
+    if not username:
+        return redirect(url_for('index'))
+
+    return render_template('loginsuccess.html', username=username)
+
+# 注册
+@app.route('/register', methods=['GET', 'POST'])
+def registerpage():
+    if request.method == 'GET':
+        return render_template('register.html')
+    nusername = request.form['username']
+    npassword = request.form['password']
+    npassword1 = request.form['password1']
+
+    print(nusername)
+
+    if npassword !=npassword1:
+        errno = '两次密码输入不一致！'
+        return render_template('register.html',errno=errno)
+    nuser = User.query.filter(User.username == nusername).first()
+    print(nuser)
+
+    if nuser is None:
+        user1 = User(username=nusername, password=npassword)
+        db.session.add(user1)
+        db.session.commit()
+        return redirect(url_for('loginpage'))
+        # return render_template('login.html')
+
+    else:
+        errno = '用户名已存在！'
+        return render_template('register.html', errno=errno)
+
+# 注销
+@app.route('/logout')
+def logoutpage():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+
+
+@app.route('/category')
+def category():
+    page = request.args.get('page', 1, type=int)
+    pagination = Category.query.order_by(-Category.add_date).paginate(page, per_page=10, error_out=False)
+    category_list = pagination.items
+    print(page)
+    print(pagination)
+    print(category_list)
+    return render_template('category.html', category_list=category_list, pagination=pagination)
+
+
+@app.route('/article')
+def article():
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.add_date).paginate(page, per_page=10, error_out=False)
+    post_list = pagination.items
+    return render_template('article.html',post_list=post_list, pagination=pagination)
+
+
+    # return render_template('register.html')
+
+
+    # if user is None:
+    #     error = '该用户不存在！'
+    #     # return redirect(url_for('loginpage'))
+    #     return render_template('login.html', errnos=error)
+    # else:
+    #     if password != user.password:
+    #         error = '密码不正确！'
+    #
+    #         # return redirect(url_for('loginpage'))
+    #         return render_template('login.html', errnos=error)
+    #     else:
+    #         session.clear()
+    #         session['user_id'] = user.id
+    #         return redirect(url_for('index'))
+
+
+    # print(error)
+
+
+    # return render_template('login.html')
+
+
+
+
+if __name__ == '__main__':
+    # app = create_app()
+    app.run()
---
+++ models.py
@@ -0,0 +1,63 @@
+# models.py
+from ext import db
+from datetime import datetime
+from sqlalchemy.dialects.mysql import LONGTEXT
+from enum import IntEnum
+
+
+# 创建ORM模型类
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    username = db.Column(db.String(50),nullable=False)
+    # addresses = db.relationship('Address', backref='user')
+    password = db.Column(db.String(100),nullable=False)
+
+
+class Category(db.Model):
+    """分类模型
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    icon = db.Column(db.String(128), nullable=True)
+    post = db.relationship('Post', backref='category', lazy=True)
+    add_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, )  # 创建时间
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)  # 更新时间
+
+
+class PostPublishType(IntEnum):
+    """ 文章发布类型
+    """
+    draft = 1    # 草稿
+    show = 2     # 发布
+
+# 多对多关系帮助器表
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
+
+
+class Post(db.Model):
+    """文章模型
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    desc = db.Column(db.String(200), nullable=True)        # 文章简介
+    has_type = db.Column(db.Enum(PostPublishType), server_default='show', nullable=False)  # 文章类型（草稿, 发布）
+    # 一对多关系
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)  #归属分类id
+    content = db.Column(LONGTEXT, nullable=False)
+    # 多对多关系
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('post', lazy=True))
+
+    add_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, )  # 创建时间
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)  # 更新时间
+
+class Tag(db.Model):
+    """ 文章标签
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    add_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, )  # 创建时间
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)  # 更新时间
+
